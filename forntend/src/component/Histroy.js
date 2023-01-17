f669fd876206221926f0b11d99c2468239a58d37
Oluwadebo <ogunweoluwadebo@gmail.com>
2023-01-17 13:36:57 +0100

    history is displayed

--- forntend/src/component/Histroy.js
+++ forntend/src/component/Histroy.js
@@ -7,10 +7,11 @@ import svgexport1 from './assets/pic/svgexport-1.png'
 import { Link } from 'react-router-dom'
 
 const Histroy = () => {
-    const [histo, sethisto] = useState([])
     const navigate = useNavigate()
     const bank = localStorage.bank;
     const customerId = localStorage.customerId;
+    const [customers, setcustomers] = useState({})
+    const [histo, sethisto] = useState([])
     const [mess, setmess] = useState("")
 
     useEffect(() => {
@@ -26,6 +27,7 @@ const Histroy = () => {
                     if (data) {
                         let Err = data.data.message;
                         if (Err == "Valid Token") {
+                            setcustomers(data.data.result[0]);
                             axios.post(`${baseUrl}gethistory`, { customerId }).then((data) => {
                                 if (data) {
                                     let history = data.data.message;
@@ -80,10 +82,43 @@ const Histroy = () => {
             </nav>
             <div className="my-5">
                 <div className="container py-1 py-md-5">
-                    <div className='shadow p-5 my-md-5 my-0' style={{ background: "rgba(255,255,255,.2)", backdropFilter: "blur(5px)", borderRadius: "5px" }}>
-                        <h3 className='alig'>{mess}</h3>
-                        
+                    <div className="row my-3 p-3 shadow ad">
+                        <div className="col-12 col-md-6">
+                            <div className="row">
+                                <div className="col-9">
+                                    <p className='pt-2'>Account Number</p>
+                                </div>
+                                <div className="col-3">
+                                    <h5 className='pt-2' style={{ float: 'right' }}>{customers.accountNumber}</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <div className="row">
+                                <div className="col-8">
+                                    <p className='pt-2'>Total Balance</p>
+                                </div>
+                                <div className="col-4">
+                                    <h5 className='pt-2' style={{ float: 'right' }}><b>₦</b> {customers.balance}</h5>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <h3 className='alig'>{mess}</h3>
+                <div className="container py-1 py-md-5">
+                    {histo.map((item, index) => (
+                        <div className='shadow p-5 my-md-1 my-0' style={{ background: "rgba(255,255,255,.2)", backdropFilter: "blur(5px)", borderRadius: "5px" }}>
+                            <div className="row">
+                                <div className="col-6">
+                                    <h4>{item.transactiontime}</h4>
+                                </div>
+                                <div className="col-6">
+                                    <h4 style={{ float: 'right', color: "green" }}>+ ₦{item.added}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
